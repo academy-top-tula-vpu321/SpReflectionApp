8dc807accbf59cd82da30732bd1d5181e7ee1da8
maxefimov <efimov@mail.ru>
2024-09-22 13:18:07 +0300

    Добавьте файлы проекта.

---
+++ SpReflectionApp/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpReflectionApp
+{
+    public class Employee : IStuddable, IWorkable
+    {
+        string? name;
+        int age;
+        public string? Name
+        {
+            set => name = value;
+            get => name;
+        }
+        public int Age
+        {
+            set => age = value;
+            get => age;
+        }
+        internal Employee()
+        {
+            name = "Anonim";
+            age = 0;
+        }
+
+        public Employee(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void Study()
+        {
+            Console.WriteLine($"{Name} studying");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine($"{Name} working");
+        }
+    }
+
+    interface IStuddable
+    {
+        void Study();
+    }
+
+    interface IWorkable
+    {
+        void Work();
+    }
+}
---
+++ SpReflectionApp/Examples.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpReflectionApp
+{
+    static class Examples
+    {
+        public static void GetTypesInfo()
+        {
+            int number = 100;
+
+            Type? type = typeof(Employee); //Type.GetType("employee", false, true); // typeof(Int32); //number.GetType();
+            if (type is not null)
+            {
+                Console.WriteLine(type.Name);
+                Console.WriteLine(type.Assembly);
+                Console.WriteLine(type.Namespace);
+                Console.WriteLine(type.IsClass);
+                Console.WriteLine(type.IsValueType);
+
+                Console.WriteLine("\nInterfaces:");
+                foreach (var inter in type.GetInterfaces())
+                    Console.WriteLine(inter.Name);
+
+            }
+        }
+
+        public static void GetMembersInfo()
+        {
+            Type employeeType = typeof(Employee);
+
+            foreach (MemberInfo member in employeeType.GetMembers(BindingFlags.Public |
+                                                                 BindingFlags.NonPublic |
+                                                                 BindingFlags.Instance |
+                                                                 BindingFlags.DeclaredOnly))
+                Console.WriteLine($"{member.DeclaringType}\t{member.MemberType}\t{member.Name}");
+            Console.WriteLine();
+
+
+            MemberInfo[] nameProp = employeeType.GetMember("Name");
+            foreach (MemberInfo member in nameProp)
+                Console.WriteLine($"{member.DeclaringType}\t{member.MemberType}\t{member.Name}");
+        }
+    }
+}
