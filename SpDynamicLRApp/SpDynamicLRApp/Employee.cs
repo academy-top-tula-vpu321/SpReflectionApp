397ba2fc7db7295cd8fa556cd32028ccd07ef9a8
maxefimov <efimov.top.academy@gmail.com>
2024-09-29 10:51:46 +0300

    29-09-24

---
+++ SpDynamicLRApp/SpDynamicLRApp/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpDynamicLRApp
+{
+    public class Employee
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+        public dynamic GetSalary(string format)
+        {
+            if (format == "string")
+                return Salary.ToString();
+            else
+                return Salary;
+        }
+    }
+
+    public class EmployeeDynamic : DynamicObject
+    {
+        Dictionary<string, object> memebers = new();
+
+        public override bool TrySetMember(SetMemberBinder binder, object? value)
+        {
+            if(value is not null)
+            {
+                memebers[binder.Name] = value;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object? result)
+        {
+            result = null;
+            if(memebers.ContainsKey(binder.Name))
+            {
+                result = memebers[binder.Name];
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object?[]? args, out object? result)
+        {
+            result = null;
+
+            if (args?.Length == 0)
+            {
+                dynamic method = memebers[binder.Name];
+                method();
+                return true;
+            }
+            else if (args?[0] is int number)
+            {
+                dynamic method = memebers[binder.Name];
+                result = method(number);
+            }
+
+            return result is not null;
+        }
+    }
+}
---
+++ SpDynamicLRApp/SpDynamicLRApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using SpDynamicLRApp;
+using System.Dynamic;
+using System.Threading.Channels;
+
+object obj = 25;
+
+obj = "Hello world";
+obj = 10.5;
+obj = new List<string>();
+
+obj = 10;
+// obj += 5;
+
+dynamic dyn = 10;
+dyn = "Hello world";
+dyn = 10;
+dyn += 5;
+
+dynamic user = new ExpandoObject();
+
+user.Name = "Bobby";
+user.Age = 25;
+user.Cities = new List<string>() { "Moscow", "Tula", "Orel" };
+
+user.Info = (Action)(() =>
+{
+    Console.Write($"User name: {user.Name}, age: {user.Age}, cities: ");
+    foreach (var c in user.Cities)
+        Console.Write($"{c} ");
+    Console.WriteLine();
+});
+
+user.Info();
+
+
+dynamic person = new EmployeeDynamic();
+person.Name = "Tommy";
+person.Age = 30;
+person.AgeAdd = (Func<int, int>)((int diff) => person.Age += diff);
+person.Info = (Action)(() => Console.WriteLine($"Name: {person.Name}, Age: {person.Age}"));
+
+person.Info();
+person.AgeAdd(5);
+person.Info();
+
+
+
