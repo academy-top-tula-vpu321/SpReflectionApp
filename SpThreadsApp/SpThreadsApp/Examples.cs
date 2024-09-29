4c33cab462bc8b8f724ca94c6b4aafc50b71c2a2
maxefimov <efimov.top.academy@gmail.com>
2024-09-29 12:19:45 +0300

    29-09-24

---
+++ SpThreadsApp/SpThreadsApp/Examples.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpThreadsApp
+{
+    static class Examples
+    {
+        static public void ThreadWelcome()
+        {
+            Thread threadCurrent = Thread.CurrentThread;
+
+            Console.WriteLine($"Name: {threadCurrent.Name}");
+            threadCurrent.Name = "Main";
+            Console.WriteLine($"Name: {threadCurrent.Name}");
+
+            Console.WriteLine($"Is Alive?: {threadCurrent.IsAlive}");
+            Console.WriteLine($"Id: {threadCurrent.ManagedThreadId}");
+            Console.WriteLine($"Priority: {threadCurrent.Priority}");
+            Console.WriteLine($"Status: {threadCurrent.ThreadState}");
+
+            //Thread threadTwo = new(ActionMethod);
+            //Thread threadTwo = new(new ThreadStart(ActionMethod));
+            Thread threadTwo = new(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}: {i}");
+                    Thread.Sleep(1000);
+                }
+            });
+            Thread threadThree = new(new ParameterizedThreadStart(ParamLoop));
+            threadThree.Start(15);
+
+            threadTwo.Start();
+            ActionMethod();
+
+
+            void ActionMethod()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}: {i}");
+                    Thread.Sleep(1000);
+                }
+            }
+
+            void ParamLoop(object? size)
+            {
+                if (size is int sizeLoop)
+                    for (int i = 0; i < sizeLoop; i++)
+                    {
+                        Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}: {i}");
+                        Thread.Sleep(1000);
+                    }
+            }
+        }
+    }
+}
---
+++ SpThreadsApp/SpThreadsApp/Program.cs
@@ -0,0 +1,89 @@
+﻿//int count = 0;
+
+//object locker = new();
+//Mutex mutex = new();
+//AutoResetEvent handler = new(true);
+
+//for(int i = 0; i < 5; i++)
+//{
+//    Thread thread = new(Increment);
+//    thread.Start();
+//}
+
+for(int i = 0; i < 10; i++)
+{
+    Car car = new();
+}
+
+
+
+
+//void Increment()
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        //lock (locker)
+//        //mutex.WaitOne();
+//        handler.WaitOne();
+//        count++;
+//        handler.Set();
+//        //mutex.ReleaseMutex();
+        
+
+//        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {count}");
+//        Thread.Sleep(100);
+//    }
+
+//    //bool lockTaken = false;
+//    //try
+//    //{
+//    //    Monitor.Enter(locker, ref lockTaken);
+//    //    for (int i = 0; i < 10; i++)
+//    //    {
+//    //        //lock (locker)
+
+//    //        count++;
+//    //        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {count}");
+//    //        Thread.Sleep(100);
+//    //    }
+//    //}
+//    //finally
+//    //{
+//    //    if (lockTaken) Monitor.Exit(locker);
+//    //}
+//}
+
+class Car
+{
+    static int globalId = 0;
+    static Semaphore semaphore = new(5, 5);
+
+    Thread thread;
+    int count = 10;
+
+    public Car()
+    {
+        thread = new Thread(Parking);
+        thread.Name = $"Car {++globalId}";
+        thread.Start();
+        count = 10;
+    }
+
+    public void Parking()
+    {
+        while(this.count > 0)
+        {
+            semaphore.WaitOne();
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} to parking...");
+            Thread.Sleep(500);
+            Console.WriteLine($"{Thread.CurrentThread.Name} out from parking...");
+            this.count--;
+
+            semaphore.Release();
+            Thread.Sleep(500);
+        }
+        
+    }
+
+}
