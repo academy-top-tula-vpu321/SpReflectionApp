e7352c50596b8e2898103a887607663b0bbddbd8
maxefimov <efimov.top.academy@gmail.com>
2024-10-06 11:10:45 +0300

    06-10-24

---
+++ SpParallelLinqApp/SpParallelLinqApp/Program.cs
@@ -0,0 +1,20 @@
+﻿Random random = new();
+List<int> array = new();
+
+for (int i = 0; i < 20; i++)
+    array.Add(i + 1);
+
+var squares = from item in array.AsParallel()
+                                .AsOrdered()
+              where item % 2 == 0  
+              //orderby item
+              select Square(item);
+
+//array.AsParallel()
+//     .Select(item => Square(item))
+//     .ForAll(Console.WriteLine);
+
+foreach (var square in squares)
+    Console.WriteLine(square);
+
+int Square(int x) => x * x;
